fc5c514230555c2892cedd83f93981a2d65f04e3
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-06-21 14:49:22 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
 
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
