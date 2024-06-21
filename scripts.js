91f0438d4cf1b374d96cf80ddd681ee6f1a944fa
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-06-21 15:26:16 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
-document.querySelector('.menutoggle').addEventListener('click', () => {
-    document.querySelector('.menu').classList.toggle('open');
+    <!-- JavaScript code to handle menu toggle and hover sounds -->
+    <script>
+$(document).ready(function() {
+  let menuToggle = document.querySelector(".menuToggle");
+  let menu = document.querySelector(".menu");
+
+  menuToggle.onclick = function () {
+    menu.classList.toggle("active");
+  };
 });
 
+    </script>
+
+    <!-- Ionicons for menu icons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
